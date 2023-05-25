f391f2ecf8d5d635dfc65c22f37dde8058e2d1a0
Lucas Lofaro <lucasmlofaro@gmail.com>
2023-05-25 00:44:40 -0700

    make salience agent inherit form base agent

--- src/agentforge/agent/salience_agent.py
+++ src/agentforge/agent/salience_agent.py
@@ -1,40 +1,26 @@
-from .func.agent_functions import AgentFunctions
-from .summarization_agent import SummarizationAgent
+from .agent import Agent
 from .execution_agent import ExecutionAgent
-from ..logs.logger_config import Logger
-
-logger = Logger(name="Salience Agent")
+from .summarization_agent import SummarizationAgent
 
 
-class SalienceAgent:
-    agent_data = None
-    agent_funcs = None
-    storage = None
-    spinner_thread = None
-    summarization_agent = None
-    exec_agent = None
-
+class SalienceAgent(Agent):
     def __init__(self):
-        self.agent_funcs = AgentFunctions('SalienceAgent')
-        self.agent_data = self.agent_funcs.agent_data
-        self.storage = self.agent_data['storage'].storage_utils
+        super().__init__('SalienceAgent', log_level="info")
 
         # Summarize the Search Results
         self.summarization_agent = SummarizationAgent()
         self.exec_agent = ExecutionAgent()
 
-        logger.set_level('info')
-
     def run_salience_agent(self, feedback=None):
 
-        logger.log(f"Running Agent...", 'info')
+        self.logger.log(f"Running Agent...", 'info')
         # Load Last Results and Current Task as Data
         data = self.load_data_from_storage()
 
         # Feed Data to the Search Utility
         search_results = self.storage.query_db("results", data['current_task']['document'], 5)['documents']
 
-        logger.log(f"Search Results: {search_results}", 'info')
+        self.logger.log(f"Search Results: {search_results}", 'info')
 
         # Summarize the Search Results
         if search_results == 'No Results!':
@@ -42,7 +28,7 @@ class SalienceAgent:
         else:
             context = self.summarization_agent.run_summarization_agent(search_results)
 
-        # logger.log(f"Summary of Results: {context}", 'info')
+        # self.logger.log(f"Summary of Results: {context}", 'info')
 
         task_result = self.exec_agent.run_execution_agent(context=context, feedback=feedback)
 
@@ -60,9 +46,9 @@ class SalienceAgent:
             "task_order": data['task_order']
         }
 
-        logger.log(f"Execution Results: {execution_results}", 'debug')
+        self.logger.log(f"Execution Results: {execution_results}", 'debug')
 
-        logger.log(f"Agent Done!", 'info')
+        self.logger.log(f"Agent Done!", 'info')
         return execution_results
 
     def load_data_from_storage(self):
@@ -72,7 +58,7 @@ class SalienceAgent:
         })
         result = result_collection[0] if result_collection else ["No results found"]
 
-        logger.log(f"Load Data Results:\n{result}", 'debug')
+        self.logger.log(f"Load Data Results:\n{result}", 'debug')
 
         task_collection = self.storage.load_salient({
             'collection_name': "tasks",
@@ -80,7 +66,7 @@ class SalienceAgent:
             'ids': "ids"
         })
 
-        logger.log(f"Tasks Before Ordering:\n{task_collection}", 'debug')
+        self.logger.log(f"Tasks Before Ordering:\n{task_collection}", 'debug')
         # quit()
 
         # first, pair up 'ids', 'documents' and 'metadatas' for sorting
@@ -100,15 +86,15 @@ class SalienceAgent:
             'metadatas': list(sorted_metadatas),
         }
 
-        logger.log(f"Tasks Ordered list:\n{ordered_list}", 'debug')
+        self.logger.log(f"Tasks Ordered list:\n{ordered_list}", 'debug')
 
-        logger.log(f"Tasks IDs:\n{sorted_ids}", 'debug')
+        self.logger.log(f"Tasks IDs:\n{sorted_ids}", 'debug')
 
         current_task = None
         # iterate over sorted_metadatas
         for i, metadata in enumerate(sorted_metadatas):
             # check if the task_status is not completed
-            logger.log(f"Sorted Metadatas:\n{metadata}", 'debug')
+            self.logger.log(f"Sorted Metadatas:\n{metadata}", 'debug')
             if metadata['task_status'] == 'not completed':
                 current_task = {
                     'id': sorted_ids[i],
@@ -118,11 +104,11 @@ class SalienceAgent:
                 break  # break the loop as soon as we find the first not_completed task
 
         if current_task is None:
-            logger.log("Task list has been completed!!!", 'info')
+            self.logger.log("Task list has been completed!!!", 'info')
             quit()
 
-        logger.log(f"Current Task:{current_task['document']}", 'info')
-        logger.log(f"Current Task:\n{current_task}", 'debug')
+        self.logger.log(f"Current Task:{current_task['document']}", 'info')
+        self.logger.log(f"Current Task:\n{current_task}", 'debug')
 
         ordered_results = {
             'result': result,
@@ -133,45 +119,3 @@ class SalienceAgent:
         }
 
         return ordered_results
-
-    def get_prompt_formats(self, data):
-        # Create a dictionary of prompt formats based on the loaded data
-        prompt_formats = {
-            'SystemPrompt': {'objective': self.agent_data['objective']},
-            'ContextPrompt': {'context': data['context']},
-            'InstructionPrompt': {'task': data['task']}
-        }
-        return prompt_formats
-
-    def generate_prompt(self, prompt_formats, feedback=None):
-        # Load Prompts
-        system_prompt = self.agent_data['prompts']['SystemPrompt']
-        context_prompt = self.agent_data['prompts']['ContextPrompt']
-        instruction_prompt = self.agent_data['prompts']['InstructionPrompt']
-        feedback_prompt = self.agent_data['prompts']['FeedbackPrompt'] if feedback != "" else ""
-
-        # Format Prompts
-        system_prompt = system_prompt.format(**prompt_formats.get('SystemPrompt', {}))
-        context_prompt = context_prompt.format(**prompt_formats.get('ContextPrompt', {}))
-        instruction_prompt = instruction_prompt.format(**prompt_formats.get('InstructionPrompt', {}))
-        feedback_prompt = feedback_prompt.format(feedback=feedback)
-
-        prompt = [
-            {"role": "system", "content": f"{system_prompt}"},
-            {"role": "user", "content": f"{context_prompt}{instruction_prompt}{feedback_prompt}"}
-        ]
-
-        # print(f"\nPrompt: {prompt}")
-        return prompt
-
-    def execute_task(self, prompt):
-        # Execute the main task of the agent and return the result
-        pass
-
-    def save_results(self, result):
-        # Save the results to storage
-        pass
-
-    # def show_tasks(self, agent):
-    #     print("")
-    #     logger.log(f"Agent {agent}", 'info')
