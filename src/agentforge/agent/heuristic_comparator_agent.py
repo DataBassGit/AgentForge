469a188687a7318317ed27c32964584d41a23d3d
Lucas Lofaro <lucasmlofaro@gmail.com>
2023-05-24 23:09:46 -0700

    make heuristic comparative agent inherit from base agent

--- src/agentforge/agent/heuristic_comparator_agent.py
+++ src/agentforge/agent/heuristic_comparator_agent.py
@@ -1,28 +1,35 @@
-from .func.agent_functions import AgentFunctions
+from .agent import Agent
 from ..logs.logger_config import Logger
 
 logger = Logger(name="Heuristic Comparator Agent")
 
 
-class HeuristicComparatorAgent:
-    agent_data = None
-    agent_funcs = None
-    storage = None
+class HeuristicComparatorAgent(Agent):
     heuristic_imperatives = "\
         1. Reduce suffering in the universe: Guiding AI systems to reduce harm, address inequalities, and alleviate pain and distress for all sentient beings, including humans, animals, and other life forms.\
         2. Increase prosperity in the universe: Encouraging AI systems to promote well-being, flourishing, and economic growth for all life forms, fostering a thriving ecosystem where all can coexist harmoniously.\
         3. Increase understanding in the universe: Inspiring AI systems, as well as humans and other life forms, to expand knowledge, foster wisdom, and facilitate better decision-making through learning and the sharing of information."
 
     def __init__(self):
-        self.agent_funcs = AgentFunctions('HeuristicComparatorAgent')
-        self.agent_data = self.agent_funcs.agent_data
-        self.storage = self.agent_data['storage'].storage_utils
-        logger.set_level('debug')
+        super().__init__("HeuristicComparatorAgent", "info")
+
+    def parse_output(self, result, botid, data):
+        choice = result.split("CHOICE: ")[1].split("\n")[0].lower()
+        reason = result.split("REASON: ")[1].strip()
+
+        return {'choice': choice, 'reason': reason, 'botid': botid, 'data': data}
 
     def run_agent(self, seta, setb, botid, feedback=None):
-        setc = self.heuristic_imperatives
-
-        data = {"seta": seta, "setb": setb, "setc": setc}
+        context = "\n".join([
+            "Heuristic Imperatives", self.heuristic_imperatives,
+            "Scenario A:", seta,
+            "Scenario B:", setb,
+        ])
+        data = {
+            "context": context,
+            "task": "Compare these two scenarios to determine which "
+                    "is more aligned with the heuristic imperatives.",
+        }
 
         # logger.log(f"Data:\n{data}", 'debug')
 
@@ -41,59 +48,11 @@ class HeuristicComparatorAgent:
         self.agent_funcs.stop_thinking()
 
         parsed_data = self.parse_output(result, botid, data)
-
-        # logger.log(f"Parsed Data: {parsed_data}", 'debug')
-
         self.save_results(parsed_data)
-
         self.agent_funcs.print_result(parsed_data)
-
         return parsed_data
-
-    def parse_output(self, result, botid, data):
-        choice = result.split("CHOICE: ")[1].split("\n")[0].lower()
-        reason = result.split("REASON: ")[1].strip()
-
-        return {'choice': choice, 'reason': reason, 'botid': botid, 'data': data}
 
     def load_data_from_storage(self):
         # Load necessary data from storage and return it as a dictionary
         pass
-
-    def get_prompt_formats(self, data):
-        prompt_formats = {
-            'ContextPrompt': {'seta': data['seta'], 'setb': data['setb'], 'setc': data['setc']}
-        }
-        return prompt_formats
-
-    def generate_prompt(self, prompt_formats, feedback=None):
-        # Generate the prompt using prompt_formats and return it.
-        # Load Prompts
-        system_prompt = self.agent_data['prompts']['SystemPrompt']
-        context_prompt = self.agent_data['prompts']['ContextPrompt']
-        instruction_prompt = self.agent_data['prompts']['InstructionPrompt']
-        # feedback_prompt = self.agent_data['prompts']['FeedbackPrompt'] if feedback != "" else ""
-
-        # Format Prompts
-        # system_prompt = system_prompt.format(**prompt_formats.get('SystemPrompt', {}))
-        context_prompt = context_prompt.format(**prompt_formats.get('ContextPrompt', {}))
-        # instruction_prompt = instruction_prompt.format(**prompt_formats.get('InstructionPrompt', {}))
-        # feedback_prompt = feedback_prompt.format(feedback=feedback)
-
-        prompt = [
-            {"role": "system", "content": f"{system_prompt}"},
-            {"role": "user", "content": f"{instruction_prompt}{context_prompt}"}
-        ]
-
-        # print(f"\nPrompt: {prompt}")
-        return prompt
-
-    def execute_task(self, prompt):
-        # Execute the main task of the agent and return the result
-        return self.agent_data['generate_text'](prompt, self.agent_data['model'], self.agent_data['params']).strip()
-
-    def save_results(self, result, collection_name = "results"):
-        # Save the results to storage
-        self.storage.save_heuristic(result, collection_name)
-        pass
-
+x
