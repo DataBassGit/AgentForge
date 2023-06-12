36d6902f153bbb6aba518bc0df4396d60e65b0e7
Lucas Lofaro <lucasmlofaro@gmail.com>
2023-06-12 04:29:09 -0700

    update oobabooga to conform to protocol

--- src/agentforge/llm/oobabooga.py
+++ src/agentforge/llm/oobabooga.py
@@ -1,41 +1,34 @@
 import requests
 
-# Server address
-server = "127.0.0.1"
 
+class Oobabooga:
+    _server = "127.0.0.1"
 
-def generate_text(prompt, model, params):
-    print("prompt:" + prompt)
-    # print("\nparams:" + str(params + "\n"))
+    def generate_text(self, prompt, params):
+        print("prompt:" + prompt)
 
-    with requests.Session() as session:
-        response = session.post(f"http://{server}:7860/run/textgen", json={
-            "data": [
-                prompt,
-                params['max_new_tokens'],
-                params['do_sample'],
-                params['temperature'],
-                params['top_p'],
-                params['typical_p'],
-                params['repetition_penalty'],
-                params['encoder_repetition_penalty'],
-                params['top_k'],
-                params['min_length'],
-                params['no_repeat_ngram_size'],
-                params['num_beams'],
-                params['penalty_alpha'],
-                params['length_penalty'],
-                params['early_stopping'],
-                params['seed']
-            ]
-        }).json()
+        data = [
+            prompt,
+            params['max_new_tokens'],
+            params['do_sample'],
+            params['temperature'],
+            params['top_p'],
+            params['typical_p'],
+            params['repetition_penalty'],
+            params['encoder_repetition_penalty'],
+            params['top_k'],
+            params['min_length'],
+            params['no_repeat_ngram_size'],
+            params['num_beams'],
+            params['penalty_alpha'],
+            params['length_penalty'],
+            params['early_stopping'],
+            params['seed']
+        ]
+        with requests.Session() as session:
+            response = session.post(f"http://{self._server}:7860/run/textgen",
+                                    json={"data": data}).json()
+            print(response)
+            reply = response["data"][0]
 
-        # debug
-        print(response)
-
-        reply = response["data"][0]
-
-    # Close the session
-    # session.close()
-
-    return reply
+        return reply
