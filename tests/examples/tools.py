6f7ec30bb54c8d464d66bce860e8a31e614e4fc0
anselale <66854855+anselale@users.noreply.github.com>
2023-07-03 20:31:32 -0600

    Got embedding query working
    Added a query_embeeding function that actually works

--- tests/examples/tools.py
+++ tests/examples/tools.py
@@ -2,11 +2,13 @@ from agentforge.utils.storage_interface import StorageInterface
 
 query = "The 'Intelligent Chunk' tool splits a provided text into smaller, manageable parts or 'chunks'. The user decides the size of these chunks based on their needs."
 
+# query = "Lesbians"
+
 storage = StorageInterface().storage_utils
 
 print(storage.return_embedding_function())
 
-test_emb = storage.return_embedding(query)
+test_emb = storage.return_embedding([query])
 
 params = {
     "collection_name": 'tools',
