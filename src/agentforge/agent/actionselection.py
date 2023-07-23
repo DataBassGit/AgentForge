216a8822220017c4d0e63f972e46a0511f68650e
anselale <66854855+anselale@users.noreply.github.com>
2023-07-23 17:43:37 -0600

    Some changes to Action Selection
    Some changes to Action Selection

--- src/agentforge/agent/actionselection.py
+++ src/agentforge/agent/actionselection.py
@@ -4,26 +4,22 @@ from .. import config
 
 class ActionSelectionAgent(Agent):
 
-    def get_completed_tasks(self):
-        task_list = self.get_task_list()
+    def parse_output(self, result, bot_id, data):  # Remember to incorporate bot_if and data later on
+        params = {
+            "collection_name": 'Actions',
+            "query": result,
+            "threshold": 0.7,  # optional
+            "num_results": 1,  # optional
+        }
 
-        completed_tasks = [task for task in task_list["metadatas"] if task["Status"] == "completed"]
+        search = self.storage.search_storage_by_threshold(params)
 
-        if completed_tasks:
-            # Sort tasks by order
-            # completed_tasks.sort(key=lambda x: x["Order"])
-            _order_task_list(completed_tasks)
+        return {"result": f"{search['documents']}"}
 
-            # Generate a string for the completed tasks
-            task_str = "\n".join([f"{task['Order']}. {task['Description']}" for task in completed_tasks])
-            return task_str
-        else:
-            return None
 
     def load_additional_data(self, data):
         # Add 'objective' to the data
         data['objective'] = self.agent_data.get('objective')
-        data['task_list'] = self.get_completed_tasks()
         data['task'] = self.load_current_task()['task']
 
         _show_task(data)
