ca00564a41feb84221a1d379b45553bc605d00f5
DataBass <josmith@junipercommerce.com>
2023-07-30 13:02:41 -0400

    Tool Use - full loop

---
+++ old-code/Tests/web_scrape.py
@@ -0,0 +1,15 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def get_plain_text(url):
+    # Send a GET request to the URL
+    response = requests.get(url)
+
+    # Create a BeautifulSoup object with the HTML content
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    # Extract the plain text from the HTML content
+    plain_text = soup.get_text()
+
+    return plain_text
