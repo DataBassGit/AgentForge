217ecc937af7c5bd14abc2676c2e485259a3add2
anselale <66854855+anselale@users.noreply.github.com>
2023-09-09 16:32:42 -0600

    Good Checkpoint in Salience Refactor

--- Examples/SalienceBot/salience.py
+++ Examples/SalienceBot/salience.py
@@ -20,6 +20,9 @@ class Salience:
         self.storage = StorageInterface().storage_utils
         self.logger = Logger(name="Salience")
         self.functions = Functions()
+
+        self.data = {}
+
         self.frustration = 0
         self.frustration_step = 0.1
         self.max_frustration = 0.5
@@ -28,9 +31,9 @@ class Salience:
 
     def run(self, context=None, feedback=None):
         self.log_start()
-        data = self.load_data_from_storage()
-        summary = self.summarize_task(data['current_task']['document'])
-        execution_results = self.execute_task(summary, context, feedback, data)
+        self.load_data_from_storage()
+        summary = self.summarize_task(self.data['current_task']['document'])
+        execution_results = self.execute_task(summary, context, feedback, self.data)
         self.log_results(execution_results)
         return execution_results
 
@@ -45,11 +48,10 @@ class Salience:
             self.handle_frustration(data['status'], data['reason'])
 
     def determine_current_task(self):
-        current_task = self.functions.get_current_task()
-        if current_task is None:
+        self.data['current_task'] = self.functions.get_current_task()
+        if self.data['current_task'] is None:
             self.logger.log("Task list has been completed!!!", 'info')
             quit()
-        return current_task
 
     def determine_status(self, data):
         status_results = self.status_agent.run(**data)
@@ -75,9 +77,7 @@ class Salience:
         }
 
     def fetch_ordered_task_list(self):
-        ordered_list = self.functions.get_ordered_task_list()
-        self.logger.log(f"Ordered Task List:\n{ordered_list}", 'debug')
-        return ordered_list
+        self.data['ordered_list'] = self.functions.get_ordered_task_list()
 
     def fetch_status_and_feedback(self, status_results):
         status_result = self.functions.check_status(status_results)
@@ -101,17 +101,14 @@ class Salience:
             self.frustration = 0
 
     def load_data_from_storage(self):
-        result = self.load_results()
-        ordered_list = self.fetch_ordered_task_list()
-        current_task = self.determine_current_task()
-        ordered_results = self.prepare_ordered_results(result, ordered_list, current_task)
-
-        self.logger.log(f"Ordered Results:{ordered_results}", 'info')
-        return ordered_results
+        self.load_results()
+        self.fetch_ordered_task_list()
+        self.determine_current_task()
+        self.prepare_ordered_results()
 
     def load_results(self):
-        result_collection = self.storage.load_collection({'collection_name': "Results", 'include': ["documents"]})
-        return result_collection['documents'][0] if result_collection['documents'] else "No results found"
+        results = self.storage.load_collection({'collection_name': "Results", 'include': ["documents"]})
+        self.data['result'] = results['documents'][0] if results['documents'] else "No results found"
 
     def log_results(self, execution_results):
         self.logger.log(f"Execution Results: {execution_results}", 'debug')
@@ -119,6 +116,10 @@ class Salience:
 
     def log_start(self):
         self.logger.log(f"Running Agent ...", 'info')
+
+    def prepare_ordered_results(self):
+        self.data['task_ids'] = self.data['ordered_list']['ids']
+        self.data['Order'] = self.data['current_task']["metadata"]["Order"]
 
     def run_agent(self, status_result, feedback):
         data = self.run(context=status_result, feedback=feedback)
@@ -135,16 +136,6 @@ class Salience:
         if summary is not None:
             self.functions.print_result(result=summary, desc="Summary Agent results")
         return summary
-
-    @staticmethod
-    def prepare_ordered_results(result, ordered_list, current_task):
-        return {
-            'result': result,
-            'current_task': current_task,
-            'task_list': ordered_list,
-            'task_ids': ordered_list['ids'],
-            'Order': current_task["metadata"]["Order"]
-        }
 
 
 if __name__ == '__main__':
