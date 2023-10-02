fa9686c19579298c87aeafabc01ef42204c43109
DataBass <josmith@junipercommerce.com>
2023-10-02 16:46:56 -0400

    Built structure of Chatbot example
    Added GUI code, but not implemented yet.
    Gui will be a commandline command 'agentforge gui'

---
+++ src/agentforge/utils/guiutils/listenforui.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, jsonify
+
+
+class BotApi:
+    def __init__(self, callback=None):
+        self.app = Flask(__name__)
+        self.callback = callback
+        self.setup_routes()
+
+    def setup_routes(self):
+        @self.app.route('/bot', methods=['POST'])
+        def bot_endpoint():
+            data = request.json
+            message = data.get('message', '')
+
+            # Trigger the callback function if it's set
+            if self.callback:
+                self.callback(message)
+
+            return jsonify({"received_message": message})
+
+    def run(self, host='127.0.0.1', port=1337):
+        self.app.run(host=host, port=port)
