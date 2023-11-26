351c0cb12c225d4103235209d5267cb32da4d237
Ansel <anselanselmi@gmail.com>
2023-11-26 10:29:04 -0600

    Updated CLI installer

--- src/agentforge/utils/installer/agentforge_cli.py
+++ src/agentforge/utils/installer/agentforge_cli.py
@@ -20,10 +20,30 @@ def display_custom_help():
     print(help_message)
 
 
+# def copy_yaml_files():
+#     try:
+#         src_base_path = pkg_resources.resource_filename("agentforge.utils.installer", "")
+#         dest_base_path = ".agentforge"
+#
+#         for root, dirs, files in os.walk(src_base_path):
+#             for file in files:
+#                 if file.endswith('.yaml'):
+#                     src_file_path = os.path.join(root, file)
+#                     relative_path = os.path.relpath(root, src_base_path)
+#                     dest_directory = os.path.join(dest_base_path, relative_path)
+#                     os.makedirs(dest_directory, exist_ok=True)
+#                     dest_file_path = os.path.join(dest_directory, file)
+#                     shutil.copyfile(src_file_path, dest_file_path)
+#     except Exception as e:
+#         print(f"Error copying YAML files: {e}")
+#         sys.exit(1)
+
 def copy_yaml_files():
     try:
         src_base_path = pkg_resources.resource_filename("agentforge.utils.installer", "")
         dest_base_path = ".agentforge"
+        override_all = False
+        skip_all = False
 
         for root, dirs, files in os.walk(src_base_path):
             for file in files:
@@ -33,7 +53,39 @@ def copy_yaml_files():
                     dest_directory = os.path.join(dest_base_path, relative_path)
                     os.makedirs(dest_directory, exist_ok=True)
                     dest_file_path = os.path.join(dest_directory, file)
-                    shutil.copyfile(src_file_path, dest_file_path)
+
+                    # Check if the destination file already exists
+                    if os.path.exists(dest_file_path) and not override_all and not skip_all:
+                        print(f"The file {dest_file_path} already exists.")
+
+                        response = input(
+                            "Select an option:\n"
+                            "[Y] Override this file\n"
+                            "[N] Skip this file\n"
+                            "[A] Override all existing files without asking again\n"
+                            "[Z] Skip all existing files without asking again\n"
+                            "Enter your choice (Y/N/A/Z): "
+                        ).lower()
+
+                        if response == 'a':
+                            override_all = True
+                        elif response == 'z':
+                            skip_all = True
+                        elif response == 'y':
+                            pass
+                        elif response == 'n':
+                            print(f"Skipping {dest_file_path}")
+                            continue
+                        else:
+                            print("Invalid option. Skipping this file.")
+                            continue
+
+                    if not skip_all:
+                        shutil.copyfile(src_file_path, dest_file_path)
+                        print(f"Copied {src_file_path} to {dest_file_path}")
+                    else:
+                        print(f"Skipped {src_file_path}")
+
     except Exception as e:
         print(f"Error copying YAML files: {e}")
         sys.exit(1)
@@ -73,7 +125,8 @@ def main():
         return
 
     parser = argparse.ArgumentParser(description="AgentForge CLI", add_help=False)
-    subparsers = parser.add_subparsers(title="Commands", description="Available commands for AgentForge CLI", dest="command")
+    subparsers = parser.add_subparsers(title="Commands", description="Available commands for AgentForge CLI",
+                                       dest="command")
 
     # Define sub-commands
     init_parser = subparsers.add_parser('init', help="Copy necessary files and set up directories.")
