fa07cf58a3240ec669cf7b55b5a85610910bea3d
Ansel <anselanselmi@gmail.com>
2024-02-09 09:15:04 -0600

    Fixed duplicate logs

--- src/agentforge/utils/functions/Logger.py
+++ src/agentforge/utils/functions/Logger.py
@@ -1,6 +1,5 @@
 import os
 import logging
-from pathlib import Path
 from agentforge.utils.functions.UserInterface import UserInterface
 
 from termcolor import cprint
@@ -9,38 +8,59 @@ init(autoreset=True)
 
 
 class BaseLogger:
+    # Class-level dictionaries to track existing handlers
+    file_handlers = {}
+    console_handlers = {}
+
     def __init__(self, name='BaseLogger', log_file='default.log'):
         self.logger = logging.getLogger(name)
         self.log_file = log_file
-        self._setup_logger()
+        self._setup_file_handler()
+        self._setup_console_handler()
 
-    def _setup_logger(self):
-        if not self.logger.handlers:
-            self.logger.setLevel(logging.DEBUG)  # Default level, can be customized
-
-            # Define the relative path for the log directory
-            log_folder = "Logs"
-            # Create the Logs folder if it doesn't exist
-            if not os.path.exists(log_folder):
-                os.makedirs(log_folder)
-
-            # File handler for logs
-            log_file_path = f'{log_folder}/{self.log_file}'
-            fh = logging.FileHandler(log_file_path)
-            fh.setLevel(logging.DEBUG)  # File handler level
-
-            # Console handler for logs
-            ch = logging.StreamHandler()
-            ch.setLevel(logging.ERROR)  # Console handler level
-
-            # Formatter for handlers
-            formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-            fh.setFormatter(formatter)
-            ch.setFormatter(formatter)
-
-            # Add handlers to logger
+    def _setup_file_handler(self):
+        if self.log_file in BaseLogger.file_handlers:
+            # Use the existing file handler
+            fh = BaseLogger.file_handlers[self.log_file]
             self.logger.addHandler(fh)
+            return
+
+        self.logger.setLevel(logging.DEBUG)  # Default level, can be customized
+
+        # Define the relative path for the log directory
+        log_folder = "Logs"
+        # Create the Logs folder if it doesn't exist
+        if not os.path.exists(log_folder):
+            os.makedirs(log_folder)
+
+        # File handler for logs
+        log_file_path = f'{log_folder}/{self.log_file}'
+        fh = logging.FileHandler(log_file_path)
+        fh.setLevel(logging.DEBUG)  # Set the level for file handler
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                                      datefmt='%Y-%m-%d %H:%M:%S')
+        fh.setFormatter(formatter)
+        self.logger.addHandler(fh)
+
+        # Store the file handler in the class-level dictionary
+        BaseLogger.file_handlers[self.log_file] = fh
+
+    def _setup_console_handler(self):
+        if self.logger.name in BaseLogger.console_handlers:
+            # Use the existing console handler
+            ch = BaseLogger.console_handlers[self.logger.name]
             self.logger.addHandler(ch)
+            return
+
+        # Console handler for logs
+        ch = logging.StreamHandler()
+        ch.setLevel(logging.ERROR)  # Set the level for console handler
+        formatter = logging.Formatter('%(name)s - %(levelname)s: %(message)s\n')
+        ch.setFormatter(formatter)
+        self.logger.addHandler(ch)
+
+        # Store the console handler in the class-level dictionary
+        BaseLogger.console_handlers[self.logger.name] = ch
 
     def log_msg(self, msg, level='info'):
 
@@ -106,70 +126,6 @@ class Logger:
                 logger.set_level(level)
         else:
             self.loggers[logger_type].set_level(level)
-
-    # class Logger:
-#     def __init__(self, general_log_name='AgentForge', model_log_name='ModelIO'):
-#         self.general_logger = BaseLogger(name=general_log_name, log_file=f'{general_log_name}.log')
-#         self.model_logger = BaseLogger(name=model_log_name, log_file=f'{model_log_name}.log')
-#
-#     def log(self, msg, level='info'):
-#         level_code = self.get_level_code(level)
-#         self.general_logger.log(msg, level_code)
-#
-#     def log_prompt(self, prompt):
-#         self.model_logger.log(f'Prompt:\n{prompt}', logging.DEBUG)
-#
-#     def log_response(self, response):
-#         self.model_logger.log(f'Model Response:\n{response}', logging.DEBUG)
-#
-#     # Other methods like `parsing_error` can be adapted similarly,
-#     # depending on whether they belong to general or model logging.
-#
-#     def get_level_code(self, level):
-#         level_dict = {
-#             'debug': logging.DEBUG,
-#             'info': logging.INFO,
-#             'warning': logging.WARNING,
-#             'error': logging.ERROR,
-#             'critical': logging.CRITICAL,
-#         }
-#
-#         if level.lower() not in level_dict:
-#             self.log("Invalid log level: '{}'. Setting log level to 'info'.".format(level), 'warning')
-#             level = 'info'
-#
-#         return level_dict.get(level.lower(), logging.INFO)
-#
-#     def set_level(self, level):
-#         level_code = self.get_level_code(level)
-#         self._logger.setLevel(level_code)
-#
-#         for handler in self._logger.handlers:
-#             handler.setLevel(level_code)
-
-    # def log(self, msg, level='info'):
-    #     msg = str(msg) + '\n'  # Add a new line at the end of each message
-    #
-    #     if level == 'debug':
-    #         self._logger.debug(msg)
-    #     elif level == 'info':
-    #         self._logger.info(msg)
-    #     elif level == 'warning':
-    #         self._logger.warning(msg)
-    #     elif level == 'error':
-    #         self._logger.error(msg)
-    #         self.UI.user_input_on_error()
-    #     elif level == 'critical':
-    #         self._logger.critical(msg)
-    #         raise
-    #     else:
-    #         raise ValueError('Invalid log level: {}'.format(level))
-
-    # def log_prompt(self, prompt):
-    #     self.log(f'Prompt:\n{prompt}', 'debug')
-    #
-    # def log_response(self, response):
-    #     self.log(f'Model Response:\n{response}', "debug")
 
     def parsing_error(self, model_response, error):
         self.log(f"Parsing Error - It is very likely the model did not respond in the required "
