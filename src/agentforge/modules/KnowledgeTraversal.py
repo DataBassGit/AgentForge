df095e5f8146ee286168294b9ef9a5b16f546662
Ansel <anselanselmi@gmail.com>
2024-02-24 20:23:05 -0600

    Possible mapping implementation added

--- src/agentforge/modules/KnowledgeTraversal.py
+++ src/agentforge/modules/KnowledgeTraversal.py
@@ -13,9 +13,11 @@ class KnowledgeTraversal:
         self.storage = StorageInterface().storage_utils
 
     def query_knowledge(self, knowledge_base_name, query, metadata_map, number_results):
+        where_map = [{key, value} for key, value in metadata_map.items()]
         params = {
             "collection_name": knowledge_base_name,
             "query": query,
+            "where": where_map
         }
 
         results = self.storage.query_memory(params, number_results)
@@ -27,37 +29,5 @@ class KnowledgeTraversal:
         #     "collection_name": knowledge_base_name,
         #     "query": query,
         # }
-
-
-if __name__ == "__main__":
-    config = Config(config_path="D:\Github\AgentForge\src")
-    storage = StorageInterface().storage_utils
-    kg_name = 'KnowledgeGraph'
-    params1 = {
-        "collection_name": kg_name,
-        "data": "dogs are animals",
-        "ids": [1],
-        "metadata": [{
-            "subject": "dogs",
-            "predicate": "are",
-            "object": "animals"
-        }]
-    }
-    params2 = {
-        "collection_name": kg_name,
-        "data": "dogs are animals",
-        "ids": [1],
-        "metadata": [{
-            "subject": "cats",
-            "predicate": "are",
-            "object": "animals"
-        }]
-    }
-
-    storage.save_memory(params1)
-    storage.save_memory(params2)
-
-    data = storage.load_collection(kg_name)
-    print(data)
 
 
