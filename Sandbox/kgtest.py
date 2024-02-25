b1a680ae9082d9a30fd4da32c01e5270bdd4dc07
DataBass <johnsmith@agentforge.net>
2024-02-25 13:08:39 -0600

    Sandbox changes

---
+++ Sandbox/kgtest.py
@@ -0,0 +1,6 @@
+from agentforge.modules.KnowledgeTraversal import KnowledgeTraversal
+
+
+kg = KnowledgeTraversal()
+kg_name = 'KnowledgeGraph'
+result = kg.query_knowledge(kg_name,"what are animals",,5)
