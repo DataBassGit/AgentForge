f23637752baf5ab871cdbfcfe0b3c7abcc46421f
databass <johnsmith@agentforge.net>
2024-09-07 03:51:35 -0500

    Add chunking to Discord.

--- Sandbox/TestActions.py
+++ Sandbox/TestActions.py
@@ -1,9 +1,9 @@
 from agentforge.modules.Actions import Actions
 
-objective = 'Automate file backup'
-# context = 'Focus your scope on technology'
-context = None
-threshold = 0.5
+objective = 'Find news about the Ukraine war'
+context = 'make sure that the news is within the last 15 days. Provide links to sources'
+# context = None
+threshold = 0.9
 
 action = Actions()
 result = action.auto_execute(objective, context, threshold)
