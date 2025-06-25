13c8dfa751ee84f55461e07f0c77ae8f30c41c59
Ansel <anselanselmi@gmail.com>
2025-06-25 14:19:54 -0600

    Update to AgentForge 0.6.2
    Update to AgentForge 0.6.2

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-__version__ = "0.6.1"
+__version__ = "0.6.2"
 LICENSE = "GNU General Public License v3 or later (GPLv3+)"
 
 
@@ -11,7 +11,7 @@ def get_long_description():
 
 setup(
     name="agentforge",
-    version="0.6.1",
+    version="0.6.2",
     description="AI-driven task automation system",
     author="John Smith, Ansel Anselmi",
     author_email="contact@agentforge.net",
